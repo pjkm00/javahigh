8713d3cb41bf9ceec18390e41263a75d70856e12
pjkm00 <pjkm00@gmail.com>
2023-05-08 17:30:05 +0900

    .

--- 하이자바2/servlet01/src/kr/or/dw/basic/RequestTest.java
+++ 하이자바2/servlet01/src/kr/or/dw/basic/RequestTest.java
@@ -1,6 +1,7 @@
 package kr.or.dw.basic;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 import javax.servlet.ServletException;
@@ -31,6 +32,43 @@ public class RequestTest extends HttpServlet{
 		System.out.println("userName : " + userName);
 		System.out.println("job : " + job);
 		System.out.println("hobby : " + Arrays.toString(hobbies));
+		
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html; charset=utf-8");
+
+		PrintWriter out = res.getWriter();
+		
+		out.println("<html><head><meta charset='utf-8'><title>Request 객체 연습</title><head>");
+		out.println("<body>");
+		out.println("<h2>Request 객체 테스트 결과</h2><hr>");
+		out.println("<table border='1'>");
+		out.println("<tr><td>이름</td>");
+		out.println("<td>" + userName + "</td></tr>");
+		out.println("<tr><td>직업</td>");
+		out.println("<td>" + job + "</td></tr>");
+		out.println("<tr><td>취미</td>");
+		out.println("<td>");
+		//배열개수만큼 반복문을 이용해서 출력한다.
+		for(String hobby : hobbies) {
+			out.println(hobby + "<br>");
+		}
+		out.println("</td></tr>");
+		out.println("</table>");
+		
+		out.println("<hr>");
+		
+		out.println("<h2>Request 객체 기타 매서드 결과</h2>");
+		out.println("<table border='1'><tr><td>");
+		out.println("1. 클라이언트 IP 주소 : " + req.getRemoteAddr() + "<br>");
+		out.println("2. 요청 메서드 : " + req.getMethod() + "<br>");
+		out.println("3. ContextPath : " + req.getContextPath() + "<br>");
+		out.println("4. 프로토콜 : " + req.getProtocol() + "<br>");
+		out.println("5. 요청 URL : " + req.getRequestURL() + "<br>");
+		out.println("6. 요청 URI : " + req.getRequestURI() + "<br>");
+		out.println("</td></tr></table>");
+		
+		
+		out.println("</body></html>");
 		
 	}
 	
