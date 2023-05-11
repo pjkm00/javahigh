051b2b66072b4d42e027c08c57c2bd52c69fd54e
pjkm00 <pjkm00@gmail.com>
2023-05-11 17:27:04 +0900

    .

--- 하이자바2/quartz/src/scheduler/sumit/project/MyObj.java
+++ 하이자바2/quartz/src/scheduler/sumit/project/MyObj.java
@@ -1,7 +1,12 @@
 package scheduler.sumit.project;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -9,9 +14,21 @@ import org.quartz.JobExecutionException;
 
 public class MyObj implements Job{
 
+	HttpServletRequest req = null;
+	HttpServletResponse res = null;
+	
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		
+		OutPrint op = new OutPrint();
+		try {
+			op.doGet(req, res);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
